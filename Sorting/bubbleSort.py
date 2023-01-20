92474da307733e701b0958bb35b4e0101b8dc5cd
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2023-01-20 14:32:59 +0100

    Create bubbleSort.py

---
+++ Sorting/bubbleSort.py
@@ -0,0 +1,26 @@
+
+
+#### This file implements bubblesort
+
+def bubbleSort(A):
+  n=len(A)
+  swapped = False
+  
+  # Loop from last to first
+  for i in range(n-1,0,-1):
+    for j in range(n):
+      
+      ## Swap if element is less than next element
+      if A[j] > A[j+1]:
+        swapped = True
+        
+        A[j],A[j+1]=A[j+1],A[j]
+        
+    if not swapped:
+      ## Exit if not swapped
+      
+      return
+        
+     
+  
+      
