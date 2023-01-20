a13cff5945ea3e8489925706b211690afccefca7
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2023-01-20 22:29:16 +0100

    Update and rename insertionsort.py to insertionSort.py

---
+++ Sorting/insertionSort.py
@@ -0,0 +1,27 @@
+
+
+def insertionSort(A):
+
+    ## Loop from second to last
+    for i in range(1, len(A)):
+
+        ##Item to be placed
+        key_item = A[i]
+
+        # variable to find position
+        j = i - 1
+
+        ## Running the list in items
+        while j >= 0 and A[j] > key_item:
+
+            A[j + 1] = A[j]
+            j -= 1
+
+        ## When the item is not bigger swap
+
+        A[j + 1] = key_item
+
+
+    return A
+    
+  
