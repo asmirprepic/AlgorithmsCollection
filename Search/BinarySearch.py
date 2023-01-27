f933fbcb1dcf8e04de2502a17c4e392f6423cadb
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2023-01-28 00:24:30 +0100

    Create BinarySearch.py

---
+++ Search/BinarySearch.py
@@ -0,0 +1,24 @@
+### Binary search ###
+
+def binarySearch(A,e):
+  first = 0
+  last = len(A)-1
+  index = -1
+   
+  while(first <= last) and (index==-1):
+    mid = (first + last)//2
+    if A[mid] == e:
+      index = mid
+    else:
+      if e<A[mid]:
+        last = mid -1
+      
+      else: 
+        first = mid +1
+  return index
+
+print(linearSearch([2,2,2,2,2,3,2,2],3))  
+
+
+
+
