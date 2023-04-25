debd9ecbf314d5dc3551eb6dfbc173d77e5b8718
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2023-04-25 22:14:05 +0200

    Create selectionSort.py

---
+++ Sorting/selectionSort.py
@@ -0,0 +1,15 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Find the minimum element in the unsorted portion of the array
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        # Swap the minimum element with the first element of the unsorted portion
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+arr = [5,2,8,6,1,9]
+
+print(selection_sort(arr))
