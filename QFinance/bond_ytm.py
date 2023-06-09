0a224f8aebd29d952eca6f27cfe9c5a82bda4407
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2023-06-09 23:39:04 +0200

    Create bond_ytm.py

---
+++ QFinance/bond_ytm.py
@@ -0,0 +1,10 @@
+import scipy.optimize as optimize
+
+def bond_ytm(price,par,T,coup,freq=2,guess=0.5):
+    freq=float(freq)
+    periods = T*2
+    coupon= coup/100.*par
+    dt = [(i+1)/freq for i in range(int(periods))]
+    ytm_func = lambda y: sum([coupon/freq/(1+y/freq)**(freq*t) for t in dt]) +  par/(1+y/freq)**(freq*T) - price
+    
+    return optimize.newton(ytm_func,guess)
