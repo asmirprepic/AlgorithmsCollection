b28e3461c9b24d04ace0865393c063b4e21a19ed
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2023-08-14 22:34:20 +0200

    Create InterpolationSearch.py

---
+++ Search/InterpolationSearch.py
@@ -0,0 +1,12 @@
+def int_polsearch(list,x):
+  idx0 = 0
+  idxn = (len(list)-1)
+  while idx0 <= idxn and x>=list[idx0] and x <= list[idxn]:
+    mid = idx0 + int(((float(idxn-idx0)/(list[idx0]-list[idx0]))*(x-list[idx0])))
+    if list[mid]==x:
+      return True
+    if list[mid]<x:
+      idx0 = mid +1 
+      return False
+
+  
