3551ee43f182e398f012395ea79f9a700ef9d4d7
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2023-12-04 23:44:18 +0100

    Update vanillaCall.py

--- QFinance/vanillaCall.py
+++ QFinance/vanillaCall.py
@@ -1,61 +1,85 @@
 import numpy as np
 import math
 
+class StochasticProcess:
+    """
+    A class representing a stochastic process for modeling asset price movements.
+    """
 
-class StocahsticProcess:
-  def time_step(self):
-    
-    dW = np.random.normal(0,math.sqrt(self.delta_t))
-    dS = self.drift*self.current_asset_price*self.delta_t + self.asset_volatility*self.current_asset_price*dW
-    self.asset_prices.append(self.current_asset_price + dS)
-    self.current_asset_price = self.current_asset_price + dS
-    
+    def __init__(self, asset_price, drift, delta_t, asset_volatility):
+        """
+        Initializes the StochasticProcess class.
 
+        Parameters:
+        asset_price (float): The initial price of the asset.
+        drift (float): The drift term of the asset price, representing the expected return.
+        delta_t (float): The time increment for each step of the simulation.
+        asset_volatility (float): The volatility of the asset price.
+        """
+        self.current_asset_price = asset_price
+        self.asset_prices = [asset_price]
+        self.drift = drift
+        self.delta_t = delta_t
+        self.asset_volatility = asset_volatility
 
-  def __init__(self,asset_price,drift,delta_t,asset_volatility):
-    self.current_asset_price = asset_price
-    self.asset_prices = []
-    self.asset_prices.append(asset_price)
-    self.drift = drift
-    self.delta_t = delta_t
-    self.asset_volatility = asset_volatility
+    def time_step(self):
+        """
+        Simulates the next time step in the stochastic process.
+        """
+        dW = np.random.normal(0, math.sqrt(self.delta_t))  # Random increment based on normal distribution
+        dS = (self.drift * self.current_asset_price * self.delta_t +
+              self.asset_volatility * self.current_asset_price * dW)
+        self.current_asset_price += dS
+        self.asset_prices.append(self.current_asset_price)
 
 
 class Call:
-  def __init__(self,strike):
-    self.strike = strike
+    """
+    A class representing a call option.
+    """
 
+    def __init__(self, strike):
+        """
+        Initializes the Call class.
+
+        Parameters:
+        strike (float): The strike price of the call option.
+        """
+        self.strike = strike
 
 
 class EuroCallSim:
-  def __init__(self,Call,n_options,initial_asset_price,drift,delta_t,volatility,tte,rfr):
-    stochastic_processes = []
-    for i in range(0,n_options):
-      stochastic_processes.append(StocahsticProcess(initial_asset_price,drift,delta_t,volatility))
+    """
+    A class for simulating the price of a European call option using stochastic processes.
+    """
 
-  
-    for stochastic_process in stochastic_processes:
-      ttei = tte
-      while((ttei-stochastic_process.delta_t)>0):
-        ttei = ttei-stochastic_process.delta_t
-        stochastic_process.time_step()
-        
-      
-      
-    payoffs = []
-    for stochastic_process in stochastic_processes:
-      payoff = stochastic_process.asset_prices[len(stochastic_process.asset_prices)-1]-Call.strike
-      z = payoff if payoff > 0 else 0
-      payoffs.append(z)
+    def __init__(self, Call, n_options, initial_asset_price, drift, delta_t, volatility, tte, rfr):
+        """
+        Initializes the EuroCallSim class.
 
-    self.price = np.average(payoffs)*math.exp(-tte*rfr)
+        Parameters:
+        Call (Call): The call option to be priced.
+        n_options (int): The number of option simulations to run.
+        initial_asset_price (float): The initial price of the underlying asset.
+        drift (float): The drift term of the asset price.
+        delta_t (float): The time increment for each step of the simulation.
+        volatility (float): The volatility of the asset price.
+        tte (float): Time to expiration of the option.
+        rfr (float): Risk-free rate.
+        """
+        stochastic_processes = [StochasticProcess(initial_asset_price, drift, delta_t, volatility)
+                                for _ in range(n_options)]
 
+        for stochastic_process in stochastic_processes:
+            ttei = tte
+            while (ttei - stochastic_process.delta_t) > 0:
+                ttei -= stochastic_process.delta_t
+                stochastic_process.time_step()
 
-print(EuroCallSim(Call(130),1000,295.40,0,1/365,1.0625,36/365,0.08).price)
+        payoffs = [(max(stochastic_process.asset_prices[-1] - Call.strike, 0))
+                   for stochastic_process in stochastic_processes]
 
-    
+        self.price = np.average(payoffs) * math.exp(-tte * rfr)
 
-
-
-
-    
+# Example usage
+print(EuroCallSim(Call(130), 1000, 295.40, 0, 1/365, 1.0625, 36/365, 0.08).price)
