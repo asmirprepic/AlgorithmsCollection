93545ed0773421b46ba46513c70aeca24fbf0bfc
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2023-12-05 23:51:16 +0100

    Update bs_finite_difference.py

--- QFinance/bs_finite_difference.py
+++ QFinance/bs_finite_difference.py
@@ -1,33 +1,55 @@
 import numpy as np
 
-# Parameters
-S_max = 100            # max stock price
-K = 50                 # strike price
-T = 1.0                # time to maturity
-sigma = 0.2            # volatility
-r = 0.05               # risk-free rate
-M = 50                 # number of price steps
-N = 1000               # number of time steps
-dt = T/N               # time step size
-dS = S_max/M           # price step size
+def finite_difference_option_price(S, X, r, T, sigma, N, M, option_type):
+    """
+    Finite difference method for European option pricing.
+    
+    Parameters:
+    S: Current price of the underlying asset
+    X: Exercise price of the option
+    r: Risk-free interest rate
+    T: Time to expiration of the option (in years)
+    sigma: Volatility of the underlying asset's price
+    N: Number of time steps
+    M: Number of asset price steps
+    option_type: "call" or "put"
+    """
+    dt = T / N
+    dS = S * 2 / M
+    grid = np.zeros((M+1, N+1))
 
-# Create grid
-S = np.linspace(0, S_max, M+1)
-t = np.linspace(0, T, N+1)
-V = np.zeros((M+1, N+1))
+    # Set up the end condition
+    ST = np.linspace(0, S * 2, M+1)
+    if option_type == "call":
+        grid[:, -1] = np.maximum(ST - X, 0)
+    elif option_type == "put":
+        grid[:, -1] = np.maximum(X - ST, 0)
 
-# Boundary conditions for European Call Option
-V[:, -1] = np.maximum(S - K, 0)  # at maturity
-V[-1, :-1] = (S_max - K) * np.exp(-r * t[:-1])  # S = S_max
+    # Set up the coefficients
+    a = 0.5 * dt * (sigma**2 * np.arange(M+1)**2 - r * np.arange(M+1))
+    b = 1 - dt * (sigma**2 * np.arange(M+1)**2 + r)
+    c = 0.5 * dt * (sigma**2 * np.arange(M+1)**2 + r * np.arange(M+1))
 
-# Finite Difference Method
-for j in range(N-1, -1, -1):
-    for i in range(1, M):
-        delta = (V[i+1, j+1] - V[i-1, j+1]) / (2*dS)
-        gamma = (V[i+1, j+1] - 2*V[i, j+1] + V[i-1, j+1]) / (dS**2)
-        theta = -0.5 * sigma**2 * S[i]**2 * gamma - r * S[i] * delta + r * V[i, j+1]
-        V[i, j] = V[i, j+1] + dt * theta
+    # Iteration over time
+    for j in range(N-1, -1, -1):
+        for i in range(1, M):
+            grid[i, j] = a[i] * grid[i-1, j+1] + b[i] * grid[i, j+1] + c[i] * grid[i+1, j+1]
 
-# Option price is at the second index (S = 0)
-option_price = V[1, 0]
+    # Find the closest grid point to S
+    i = np.searchsorted(ST, S) - 1
+    # Interpolate to find the option value at S
+    option_value = grid[i, 0] + (S - ST[i]) * (grid[i+1, 0] - grid[i, 0]) / (dS)
+    return option_value
+
+# Example usage
+S = 50  # Current stock price
+X = 50  # Strike price
+r = 0.05  # Annual risk-free rate
+T = 1  # Time to maturity in years
+sigma = 0.2  # Volatility
+N = 1000  # Number of time steps
+M = 100  # Number of price steps
+option_type = "call"  # Option type
+
+option_price = finite_difference_option_price(S, X, r, T, sigma, N, M, option_type)
 print("Option Price:", option_price)
