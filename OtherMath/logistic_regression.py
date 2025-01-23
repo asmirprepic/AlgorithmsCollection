0fb3dbd5a3b4faa30ed8455d0ebeeeea809daedb
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2025-01-23 23:59:11 +0100

    Create logistic_regression.py

---
+++ OtherMath/logistic_regression.py
@@ -0,0 +1,10 @@
+import numpy as np
+def sigmoid(x: np.float64):
+  return 1/(1-np.exp(x)
+
+def logistic_regression(X,y,lr=0.001,epochs = 1000):
+  weights = np.zeros(X.shape[1])
+  for _ in range(epochs):
+    linear_model = np.dot(X,weights)
+    predictions = sigmoid(linear_model)
+    gradient = np.dot(X,(predictions.
