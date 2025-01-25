74fd69ad76f51de0c785b1bd7772ee3ddf7d6c3a
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2025-01-26 00:12:11 +0100

    Update naive_bayes.py

--- OtherMath/naive_bayes.py
+++ OtherMath/naive_bayes.py
@@ -18,4 +18,54 @@ class NaiveBayes:
     self.means: Dict[str,np.ndarray] = {}
     self.stds: Dict[str, np.ndarray] = {}
 
-  def fit(self, X: List[List[float]],y: List[float] 
+  def fit(self, X: List[List[float]],y: List[float] -> List[str]:
+     """
+        Trains the Naive Bayes classifier on the given data.
+
+        Args:
+            X: Training data, where each element is a list of features.
+            y: Labels for the training data.
+      
+    """
+    classes, class_counts = np.unique(y, return_counts = True)
+    total_samples = len(y)
+
+    "Priors"
+
+    self.priors ={cls: count/total_samples for cls, count in zip(classes,class_counts)}
+    self.means = {}
+    self.stds ={}
+
+    for cls in classes: 
+      X_cls = np.array([X[i] for i in range(len(y)) if y[i] == cls)
+      self.means[cls] = np.mean(X_cls,axis = 0)
+      self.stds[cls] = np.std[X_cls,axis = 0)
+
+
+  def predict(self, X_test: List[List[float]]) -> List[str]:
+    """  
+      Predicts the class labels for given test data
+
+      Args: 
+        X_test: test data, each element is a list of features
+
+      Returns: 
+        A list of predicted class labels
+    """
+    predictions = []
+    for x in X_test: 
+      class_probs = {}
+      for cls in self.priors: 
+        prior = self.priors[cls]
+        likelihood = np.prod(self._guassian_pdf(x, self.means[cls],self.stds[cls]))
+        class_probs[cls] = prior*likelihood
+      predictions.append(max(class_probs, key = class_probs.get))
+
+  def _gaussian_pdf
+                             
+                             
+    
+    
+
+
+
