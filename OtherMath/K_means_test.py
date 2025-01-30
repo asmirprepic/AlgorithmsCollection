04413b5dd9e64aca866530d73fd6c182e68fa48b
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2025-01-30 22:17:31 +0100

    Update K_means_test.py

--- OtherMath/K_means_test.py
+++ OtherMath/K_means_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class KMeans:
@@ -10,4 +11,28 @@ class KMeans:
 
     for _ in range(self.centroids):
       labels = np.zeros(X.shape[0])
-      
+      for i in range(X.shape[0]):
+        distances = [self.euclidean_distance(X[i], centroid) for centroid in self.centroids]
+        labels[i] = np.argmin(distance)
+
+      new_centroids = np.array([X[labels == j].mean(axis =0 ) for j in range(self.k)])
+
+      if np.all(self.centroids == new_centroids):
+        break
+
+      self.centroids = new_centroids
+    return self
+
+  def predict(self,X):
+    labels = np.zeros(X.shape[0])
+    for i in range(X.shape[0]):
+      distances = [self._euclidean_distance(X[i], centroid) for centroid in self.centroids]
+      labels[i] = np.argmin(distances)
+
+    return labels
+
+
+  def _euclidean_distance(x1,x2):
+    return np.sqrt(np.sum((x1-x2)**2))
+
+
