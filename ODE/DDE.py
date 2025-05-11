3d6f168fbdbeee08933385d653aac915293d56d5
A P <asmirprepic@gmail.com>
2025-05-11 23:59:14 +0200

    Small adjustment but this is probaly wrong

--- ODE/DDE.py
+++ ODE/DDE.py
@@ -9,15 +9,15 @@ class DDE:
     self.t0 = t0
 
   def solve(self,t_end,dt):
-    steps = int(np.ceil((t_end -self.t0)/dt))
-    times = np.linspace(self.t0,t_end,steps +1)
-    solution = np.zeros(steps +1)
-    past_values = [(self.t0-d,self.history(self.t0-d)) for d in np.linspace(0,max(self.delays),100)
+    self.steps = int(np.ceil((t_end -self.t0)/dt))
+    self.times = np.linspace(self.t0,t_end,self.steps +1)
+    self.solution = np.zeros(self.steps +1)
+    self.past_values = [(self.t0-d,self.history(self.t0-d)) for d in np.linspace(0,max(self.delays),100)]
 
-    def get_delayed_value(t):
-      if t <= self.t0:
-        return self.history(t)
-      else: 
-        interp_times, interp_values = zip(*past_values)
-        interpolator = interpr1d(interp_times,interp_values, kind = 'cubic', fill_values = 'extrapolate')
-        return interpolator(t)
+  def get_delayed_value(t):
+    if t <= self.t0:
+      return self.history(t)
+    else: 
+      interp_times, interp_values = zip(*self.past_values)
+      interpolator = interpr1d(interp_times,interp_values, kind = 'cubic', fill_values = 'extrapolate')
+      return interpolator(t)
