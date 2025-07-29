f798ca58fc469fe74f73198f4fba1b4be85baf74
A P <asmirprepic@gmail.com>
2025-07-29 22:46:48 +0200

    Testing some flocking things

--- OtherMath/flocking/Boid.py
+++ OtherMath/flocking/Boid.py
@@ -14,8 +14,26 @@ class Boid:
 
     def update_position(self, dt):
         self.position += self.velocity * dt
-        self.position %= self.config['bounds']  # wrap around
+        self.position %= self.config['bounds']  #
 
     def compute_steering(self, neighbors):
         if not neighbors:
             return
+
+        avg_vel = np.mean([b.velocity for b in neighbors], axis=0)
+        alignment = (avg_vel - self.velocity) * self.config['alignment_strength']
+
+        center_mass = np.mean([b.position for b in neighbors], axis=0)
+        cohesion = (center_mass - self.position) * self.config['cohesion_strength']
+
+
+        separation = np.zeros(2)
+        for other in neighbors:
+            diff = self.position - other.position
+            dist = np.linalg.norm(diff)
+            if dist > 0:
+                separation += diff / dist**2
+        separation *= self.config['separation_strength']
+
+        self.velocity += alignment + cohesion + separation
+        self.limit_velocity()
